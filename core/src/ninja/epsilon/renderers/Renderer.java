9f58ea2cc30cd699746389a2947b1ce67f617317
Adam Zalcman <adam.zalcman@gmail.com>
2014-06-07 18:25:56 +0200

    Add create() to Renderers.

--- core/src/ninja/epsilon/renderers/Renderer.java
+++ core/src/ninja/epsilon/renderers/Renderer.java
@@ -2,6 +2,11 @@ package ninja.epsilon.renderers;
 
 public interface Renderer {
 	/**
+	 * Create. Called in the beginning to allow you to create textures, boxes and what not.
+	 */
+	void create();
+
+	/**
 	 * Draw and play sounds.
 	 */
 	void render();
