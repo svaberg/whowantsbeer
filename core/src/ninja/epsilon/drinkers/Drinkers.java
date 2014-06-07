32122b5f0b70726f31b65fee13e05cb56f018952
Adam Zalcman <adam.zalcman@gmail.com>
2014-06-07 19:10:03 +0200

    Add GameLevel enum and update Drinkers i/face.

--- core/src/ninja/epsilon/drinkers/Drinkers.java
+++ core/src/ninja/epsilon/drinkers/Drinkers.java
@@ -1,10 +1,12 @@
 package ninja.epsilon.drinkers;
 
+import ninja.epsilon.GameLevel;
+
 public interface Drinkers {
 	/**
 	 * Hook for the drinkers called from the main loop.
 	 * This will create new drinkers, retire old ones, place orders
 	 * into the order book etc.
 	 */
-	void update();
+	void update(long curTime, GameLevel level);
 }
