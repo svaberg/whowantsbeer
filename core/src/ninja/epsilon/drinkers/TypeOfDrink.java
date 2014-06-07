95756d27c407ddd52ae27cbb04eba45461c0ade2
Dag Evensberget <dag.evensberget@gmail.com>
2014-06-07 22:30:07 +0200

    Extended TypeOfDrink

--- core/src/ninja/epsilon/drinkers/TypeOfDrink.java
+++ core/src/ninja/epsilon/drinkers/TypeOfDrink.java
@@ -1,5 +1,20 @@
 package ninja.epsilon.drinkers;
 
 public enum TypeOfDrink {
-	blondBeer, darkBeer, tequilaShot
+	blondBeer(0), darkBeer(1), tequilaShot(2);
+	
+	private int type;
+
+	private TypeOfDrink(int type) {
+		this.type = type;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString().toLowerCase();
+	}
 }
