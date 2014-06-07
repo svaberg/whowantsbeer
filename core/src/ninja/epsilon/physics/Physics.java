b50232f788843e0e97e910b58bdf2da214d90c0a
certya <zoonia.delarosa@gmail.com>
2014-06-08 00:42:55 +0200

    Added glass' type

--- core/src/ninja/epsilon/physics/Physics.java
+++ core/src/ninja/epsilon/physics/Physics.java
@@ -1,5 +1,7 @@
 package ninja.epsilon.physics;
 
+import java.io.File;
+
 public interface Physics {
 	public static interface InputCallback {
 		/**
@@ -8,10 +10,37 @@ public interface Physics {
 		void swipe(float v);
 	}
 
+	public enum GlassType {
+		GLASS_BEER,
+		GLASS_WINE,
+		GLASS_SHOT,
+		GLASS_CACTUS;
+		
+		/// ... improvement.. add it in the plugin properties file/config file.
+		final static private String basePath = "drinks";
+		final static private String basePrefix = "spritesheet_";
+		public String getPath() {
+			String answer = basePath + File.separator + basePrefix; 
+			switch(this)
+			{
+				case GLASS_BEER:return answer + "beer.atlas";
+				case GLASS_WINE:return answer + "wine.atlas";
+				case GLASS_SHOT:return answer + "shot.atlas";
+				case GLASS_CACTUS:return answer + "cactus.atlas";
+				
+			
+			};
+			return answer + "beer.atlas";
+		}
+		
+		
+	};
+	
 	public static class GlassState {
 		public float x;
 		public float y;
 		public float angle;
+		public GlassType type;
 	}
 
 	/**
