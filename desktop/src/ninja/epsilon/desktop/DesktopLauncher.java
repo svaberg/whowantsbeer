fc0679bdac4da6f0624c3c90241fb9a84ac5150a
Adam Zalcman <adam.zalcman@gmail.com>
2014-06-07 12:39:20 +0200

    Initial commit: generated by gdx-setup and gradle

---
+++ desktop/src/ninja/epsilon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ninja.epsilon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ninja.epsilon.WhoWantsBeerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WhoWantsBeerGame(), config);
+	}
+}
