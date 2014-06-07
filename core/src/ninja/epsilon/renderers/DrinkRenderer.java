ce000614731e165fc2c30a1de3d527317453b5ca
certya <zoonia.delarosa@gmail.com>
2014-06-08 01:32:52 +0200

    Half done multiple drinks indexed

--- core/src/ninja/epsilon/renderers/DrinkRenderer.java
+++ core/src/ninja/epsilon/renderers/DrinkRenderer.java
@@ -4,7 +4,12 @@
 package ninja.epsilon.renderers;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import ninja.epsilon.drinkers.TypeOfDrink;
 import ninja.epsilon.physics.Physics;
 
 import com.badlogic.gdx.Gdx;
@@ -18,13 +23,13 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 public class DrinkRenderer implements Renderer {
 	
 	@SuppressWarnings("unused")
-	private Physics refPhysics = null; 
+	private Physics refPhysics = null;
+	
+	
+	private Map<TypeOfDrink, TextureAtlas> textureIndex = new HashMap<TypeOfDrink, TextureAtlas>();
+	private Map<TypeOfDrink, Animation> animationIndex = new HashMap<TypeOfDrink, Animation>();
 	
 	private SpriteBatch spriteBatch = null;
-	private Texture texture = null;
-	
-	private TextureAtlas textureAtlas = null;
-	private Animation animation;
 	private float elapsedTime = 0;
 	
 	public DrinkRenderer(final Physics physics) {
@@ -34,9 +39,23 @@ public class DrinkRenderer implements Renderer {
 
 	@Override
 	public void create() {
-		spriteBatch = new SpriteBatch(); 
-		textureAtlas = new TextureAtlas(Gdx.files.internal("drinks" + File.separator + "spritesheet_beer.atlas"));
-		animation = new Animation(1/15f, textureAtlas.getRegions());
+		spriteBatch = new SpriteBatch();
+		
+		TypeOfDrink[] values = TypeOfDrink.values();
+		TypeOfDrink drink = null;
+		int i=0, ilen=values.length;
+		TextureAtlas textureAtlas = null;
+		Animation animation = null;
+		while(i<ilen) {
+			drink = values[i];
+			textureAtlas = new TextureAtlas(Gdx.files.internal(drink.getPath()));
+			textureIndex.put(drink, textureAtlas);
+			animation = new Animation(1/15f, textureAtlas.getRegions());
+			animationIndex.put(drink, animation);
+			++i;
+		}
+		
+		
 	}
 	
 
@@ -54,7 +73,7 @@ public class DrinkRenderer implements Renderer {
 		float yPos = 0;
 		elapsedTime += Gdx.graphics.getDeltaTime();
 		
-		spriteBatch.draw(animation.getKeyFrame(elapsedTime, true), xPos, yPos);
+		//spriteBatch.draw(animation.getKeyFrame(elapsedTime, true), xPos, yPos);
 		
         spriteBatch.end();
 	}
@@ -62,12 +81,12 @@ public class DrinkRenderer implements Renderer {
     @Override
     public void dispose() {
     	spriteBatch.dispose();
+    	
 
-		if(texture != null)
-			texture.dispose();
-		if(textureAtlas != null)
-			textureAtlas.dispose();
-		
+    	Iterator<Entry<TypeOfDrink, TextureAtlas>> it = textureIndex.entrySet().iterator();
+    	while (it.hasNext()) 
+    		it.next().getValue().dispose();
+    	
 	//	super.dispose();
     }
 
