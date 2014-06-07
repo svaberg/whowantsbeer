4c924074af006dd2ed91f9499cca202743fcb1b9
Adam Zalcman <adam.zalcman@gmail.com>
2014-06-07 17:54:08 +0200

    Add Drinkers interface.

---
+++ core/src/ninja/epsilon/Drinkers.java
@@ -0,0 +1,10 @@
+package ninja.epsilon;
+
+public interface Drinkers {
+	/**
+	 * Hook for the drinkers called from the main loop.
+	 * This will create new drinkers, retire old ones, place orders
+	 * into the order book etc.
+	 */
+	void update();
+}
