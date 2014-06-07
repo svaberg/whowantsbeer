8e6d62682f9d828a61b7ed4b2cf8baca652b8e4d
Dag Evensberget <dag.evensberget@gmail.com>
2014-06-07 18:38:14 +0200

    Some classes for the DrinkerPool.

--- core/src/ninja/epsilon/drinkers/Drinker.java
+++ core/src/ninja/epsilon/drinkers/Drinker.java
@@ -1,5 +1,47 @@
 package ninja.epsilon.drinkers;
 
-public class Drinker {
+import java.util.List;
 
+
+public class Drinker {
+	/**
+	 * Creation time in milliseconds [ms] 
+	 * Time drinker appears.
+	 */
+	long creationTime;
+
+	/**
+	 * Persistence time in milliseconds [ms] 
+	 * Patience - time until drinker leaves the bar.
+	 */
+	long persistenceTime;
+	
+	/**
+	 * Drinks being ordered by the drinker. When all drinks have been received
+	 * the drinker will be satisfied and leave the bar (and a tip!).
+	 */
+	List<DrinkOrder> drinkOrders;
+	
+	/**
+	 * Returns true if the drinker has received all orders. Otherwise returns false.
+	 */
+	boolean hasReceivedAllOrders() {
+		boolean hasReceivedAllOrders = true;
+		for (DrinkOrder drinkOrder : drinkOrders) {
+			hasReceivedAllOrders &= drinkOrder.isReceived();
+		}
+		return hasReceivedAllOrders;
+	}
+	
+	/** 
+	 * Returns true if the drinker has waited too long and thus will leave.
+	 * @param nowTime
+	 * @return true if the drinker has waited too long, otherwise false
+	 */
+	boolean hasWaitedTooLong(long nowTime) {
+		long timeWaited = nowTime - creationTime;
+		return (timeWaited > persistenceTime);
+	}
+	
+	
 }
--- core/src/ninja/epsilon/drinkers/DrinkerPool.java
+++ core/src/ninja/epsilon/drinkers/DrinkerPool.java
@@ -1,5 +1,33 @@
 package ninja.epsilon.drinkers;
 
-public class DrinkerPool {
+import java.util.List;
 
+public class DrinkerPool implements Drinkers{
+
+	/**
+	 * List of drinkers currently at the bar
+	 */
+	List<Drinker> drinkersWaiting;
+	
+	/**
+	 * Create a new drinker
+	 */
+	private void createDrinker() {
+		Drinker drinker = new Drinker();
+		drinkersWaiting.add(drinker);
+	}
+	
+	/** 
+	 * Purge the list of drinkers who have received all their orders.
+	 */
+	private void purge() {
+		for (Drinker drinker : drinkersWaiting) {
+			
+		}
+	}
+	public void update() {
+		
+	}
+	
+	
 }
